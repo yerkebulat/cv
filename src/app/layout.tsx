1d2377e6561afe30d7fbd88981294f7cafe5dff5
Yerkebulan <yerkebulantazabek@gmail.com>
2025-03-08 15:38:23 +0500

    Update favicon and apple-icon with Mars image resized to 16x16

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,24 @@
 import { Analytics } from "@vercel/analytics/react";
 import { Inter } from "next/font/google";
-
+import { Metadata } from "next";
 import "./globals.css";
-import React from "react";
 
 const inter = Inter({
   subsets: ["latin"],
   display: "swap",
+  preload: true,
 });
+
+export const metadata: Metadata = {
+  metadataBase: new URL("https://yerkebulat.github.io/cv"),
+  title: "Yerkebulan Tazabek - Resume",
+  description: "Professional resume of Yerkebulan Tazabek",
+  robots: "index, follow",
+  manifest: "/site.webmanifest",
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
 
 export default function RootLayout({
   children,
@@ -16,8 +27,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={inter.className}>
-      <body>{children}</body>
-      <Analytics />
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta httpEquiv="Content-Language" content="en" />
+        <meta name="theme-color" content="#ffffff" />
+      </head>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
